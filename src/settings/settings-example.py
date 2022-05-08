4dd5a1fd16030bfbe30c7a36aac05dbed5d3f94c
pjpawel <pawel.jan.podgorski@gmail.com>
2022-05-08 12:22:30 +0200

    add settings

---
+++ src/settings/settings-example.py
@@ -0,0 +1,10 @@
+
+PATH = 'C:\\Users\\pawelprogram\\Downloads\\Test\\'
+
+DESTIN_PATH = 'C:\\Users\\pawelprogram\\Downloads\\Test\\'
+
+# 
+MODES = {
+    "Installers": ".exe$",
+    "Programs": "(.py|.ipynb)$", 
+}
---
+++ src/settings/settings.py
@@ -0,0 +1,10 @@
+
+PATH = 'C:\\Users\\pawelprogram\\Downloads\\Test\\'
+
+DESTIN_PATH = 'C:\\Users\\pawelprogram\\Downloads\\Test\\'
+
+#Dictionary
+MODES = {
+    "Installers": ".exe$",
+    "Programs": "(.py|.ipynb)$", 
+}
